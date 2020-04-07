8180b2347b91e5ea64110e3fdf3699f7dd0ae61b
lilchoto3 <lilchoto3@gmail.com>
2020-04-06 17:10:06 -0700

    add gitignore and setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='lambdata-zacharyluck',
+    version='1.0',
+    author='Zachary Luck',
+    author_email='zacharysluck@gmail.com',
+    description='Some simple DataFrame altering functions',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/Lilchoto3/lambdata-lilchoto3',
+    packages=find_packages()
+)
